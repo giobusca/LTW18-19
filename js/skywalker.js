77f0475ba419c2f563e8152d86cd52f972b7802d
Giovanni <gio@eopmac-fbuscaglione-old.local>
2018-11-26 21:25:10 +0100

    Aggiunto il 2o gruppo delle costellazioni

--- js/skywalker.js
+++ js/skywalker.js
@@ -1,6 +1,8 @@
 function search(){
     var search_arg = document.forms["search-form"].elements["star-search"].value;
-    
+    search_arg = search_arg.toLowerCase();                                                          // Makes the whole search argument lowercase
+    search_arg = search_arg.charAt(0).toUpperCase() + search_arg.substring(1,search_arg.length);    // then capitalizes the first letter for compatibility with the .txt files
+
     alert("You searched for: "+search_arg);
     return true;
 }
