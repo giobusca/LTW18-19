118eeff5365a9bebb1c27b1ee3d5ecf78d1452b6
giobusca <giovanni.buscaglione@gmail.com>
2018-12-10 14:41:09 +0100

    Introduzione di logica AltAz + migliorie

--- js/skywalker.js
+++ js/skywalker.js
@@ -145,67 +145,93 @@ function myXOR(a,b) {
     return ( a || b ) && !( a && b );
 }
 
-function julianDay() {
+function julianCenturiesSinceJ2000() {
     var date = new Date()
     var yy = date.getUTCFullYear();
-    var mm = date.getUTCMonth();
+    var mm = date.getUTCMonth()+1;
     var dd = date.getUTCDate();
     var hh = date.getUTCHours();
     var min = date.getUTCMinutes();
     var sec = date.getUTCSeconds();
+    
+    if(mm==1 || mm ==2){
+        yy  = yy - 1;
+        mm = mm + 12;
+    }
 
-    var jdn =  (1461 * (yy + 4800 + (mm - 14)/12))/4 +(367 * (mm - 2 - 12*((mm - 14)/12)) )/12 - (3 * ((yy + 4900 + (mm - 14)/12)/100))/4 + dd - 32075;
-    var jd = jdn + (hh-12)/24 + min/1440 + sec/86400;
+    var a = Math.floor((14 - mm) / 12);
+    var y = yy + 4800 - a;
+    var m = mm + 12 * a - 3;
+    var jd = dd + Math.floor((153 * m + 2) / 5) + 365 * y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(y / 400) - 32045;
+    var jt = jd + (hh-12 + min/60.0 + sec/3600.0)/24.0;
 
-    return jd;
+    var j2000d = jt - 2451545;
+    console.log("JD: "+jd+" - J2000D: "+j2000d);
+    
+    // julian centuries since J2000.0
+    var j2000Cen = j2000d/36525.0;
+
+    console.log("JT: "+j2000Cen);
+
+    return j2000Cen;
 }
 
 function getPosition(position){
-    alert("prova posizione");
-    var declination = sessionStorage.declination;
-    var rightAscention = sessionStorage.rightAsc;
-    var lat = position.coordinates.latitude;
-    var long = position.coordinates.longitude;
-
-    var jd = juliandDay();
-    
-    var gmst = (jd - 2451545) / 36525;
-
-    var lha = gmst + long - rightAscention;
-    
-    var alt = Math.asin( Math.sin(lat)*Math.sin(declination) + Math.cos(lat)*Math.cos(declination)*Math.cos(lha) );
-    var az = Math.acos( (Math.sin(Dec) - Math.sin(lat)*Math.sin(alt)) / (Math.cos(lat)*Math.cos(alt)) );
-
-
-    alert([alt, az]);
-
-    return true;
+    var lat = position.coords.latitude;
+    var long = position.coords.longitude;
+    console.log("Lat: "+lat+"; Long: "+long);
+    getAltAz(lat, long);
 }
 
-function getAltAz(){
-    navigator.geolocation.getCurrentPosition(getPosition);
+function posError(err){
+    alert("Could not retrieve current location");
+}
 
-    /*
-    var lat = position[0];
-    var long = position[1];
-    var jd = juliandDay();
+function getAltAz(lat, long){
+    var dec = Number(sessionStorage.declination);
+    var rightAsc = Number(sessionStorage.rightAsc);
+    console.log("rightAsc:"+rightAsc+"; Dec: "+dec);
+
+    // RA to degrees
+    rightAsc = 15*(rightAsc);
+
+    var jc = julianCenturiesSinceJ2000();
+    var lmst = 24110.54841 + 8640184.812866 * jc + 0.093104 * jc*jc - 0.0000062 * jc*jc*jc + long;
+
+    // in degrees modulo 360.0
+    if (lmst > 0.0) 
+        while (lmst > 360.0) lmst = lmst - 360.0;
+    else
+        while (lmst < 0.0)   lmst = lmst + 360.0;
     
-    var gmst = (jd - 2451545) / 36525;
+    var lha = lmst - rightAsc;
+    console.log("LMST: "+lmst+" - LHA: "+lha);
 
-    var lha = gmst + long - rightAscention;
+    // convert degrees to radians
+    lha  = lha*Math.PI/180
+    dec = dec*Math.PI/180
+    lat = lat*Math.PI/180
     
-    var alt = Math.asin( Math.sin(lat)*Math.sin(declination) + Math.cos(lat)*Math.cos(declination)*Math.cos(lha) );
-    var az = Math.acos( (Math.sin(Dec) - Math.sin(lat)*Math.sin(alt)) / (Math.cos(lat)*Math.cos(alt)) );
+    var alt = Math.asin( Math.sin(lat)*Math.sin(dec) + Math.cos(lat)*Math.cos(dec)*Math.cos(lha) );
+    var az = Math.acos( ( Math.sin(dec) - Math.sin(lat)*Math.sin(alt) ) / (Math.cos(lat)*Math.cos(alt)) );
+    
+    // ALT and AZ to degrees
+    alt = alt/Math.PI * 180;
+    az = az/Math.PI * 180;
+    if(Math.sin(lha)>0) az = 360 - az;
 
-    return [alt, az]; 
-    */
+    console.log("Az: "+az+"; Alt: "+alt); 
+
 }
 
 function testVis(){
     sessionStorage.rightAsc = document.getElementById("rightAsc").value.toString();
     sessionStorage.declination = document.getElementById("declination").value.toString();
 
-    getAltAz();
-    return true;
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(getPosition, posError);
+    } else {
+        alert("Geolocation is not supported");
+    }    return false;
 }
 
